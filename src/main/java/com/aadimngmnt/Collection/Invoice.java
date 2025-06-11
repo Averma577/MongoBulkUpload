946cde0efd6912df8009c644b2b5bc8acfe9814a
Aditya <av4212473@example.com>
2025-06-12 00:13:03 +0530

    2v

--- src/main/java/com/aadimngmnt/Collection/Invoice.java
+++ src/main/java/com/aadimngmnt/Collection/Invoice.java
@@ -7,11 +7,12 @@ package com.aadimngmnt.Collection;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
-@Document(collection = "invoice")
+@Document(collection = "invoices")
 public class Invoice {
 
     @Id
@@ -22,7 +23,7 @@ public class Invoice {
     private String ledgerCode;
     private String clientCode;
     private String clientName;
-
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate date; // Consider using LocalDate if it's a proper date
     private String securityCode;
 
@@ -37,4 +38,147 @@ public class Invoice {
     private String narration;
     private String recordNumber;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUploadCode() {
+        return uploadCode;
+    }
+
+    public void setUploadCode(String uploadCode) {
+        this.uploadCode = uploadCode;
+    }
+
+    public String getEntryCode() {
+        return entryCode;
+    }
+
+    public void setEntryCode(String entryCode) {
+        this.entryCode = entryCode;
+    }
+
+    public String getEntryType() {
+        return entryType;
+    }
+
+    public void setEntryType(String entryType) {
+        this.entryType = entryType;
+    }
+
+    public String getLedgerCode() {
+        return ledgerCode;
+    }
+
+    public void setLedgerCode(String ledgerCode) {
+        this.ledgerCode = ledgerCode;
+    }
+
+    public String getClientCode() {
+        return clientCode;
+    }
+
+    public void setClientCode(String clientCode) {
+        this.clientCode = clientCode;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getSecurityCode() {
+        return securityCode;
+    }
+
+    public void setSecurityCode(String securityCode) {
+        this.securityCode = securityCode;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Double getCgst() {
+        return cgst;
+    }
+
+    public void setCgst(Double cgst) {
+        this.cgst = cgst;
+    }
+
+    public Double getSgst() {
+        return sgst;
+    }
+
+    public void setSgst(Double sgst) {
+        this.sgst = sgst;
+    }
+
+    public Double getIgst() {
+        return igst;
+    }
+
+    public void setIgst(Double igst) {
+        this.igst = igst;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getLedgerName() {
+        return ledgerName;
+    }
+
+    public void setLedgerName(String ledgerName) {
+        this.ledgerName = ledgerName;
+    }
+
+    public String getDc() {
+        return dc;
+    }
+
+    public void setDc(String dc) {
+        this.dc = dc;
+    }
+
+    public String getNarration() {
+        return narration;
+    }
+
+    public void setNarration(String narration) {
+        this.narration = narration;
+    }
+
+    public String getRecordNumber() {
+        return recordNumber;
+    }
+
+    public void setRecordNumber(String recordNumber) {
+        this.recordNumber = recordNumber;
+    }
 }
